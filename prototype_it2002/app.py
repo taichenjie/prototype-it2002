a29f664de38e401356accdce7eae96859e59dcf8
taichenjie <chenjie470@gmail.com>
2023-04-07 16:28:26 +0800

    edit port in api.ts to 2222 edit port in app.py to 2222

--- prototype_it2002/app.py
+++ prototype_it2002/app.py
@@ -35,7 +35,7 @@ def create_app():
     return app
 
 # ? The port where the debuggable DB management API is served
-PORT = 8000
+PORT = 2222
 # ? Running the flask app on the localhost/0.0.0.0, port 2222
 # ? Note that you may change the port, then update it in the view application too to make it work (don't if you don't have another application occupying it)
 if __name__ == "__main__":
@@ -44,8 +44,8 @@ if __name__ == "__main__":
     # ? Uncomment the below lines and comment the above lines below `if __name__ == "__main__":` in order to run on the production server
     # ? Note that you may have to install waitress running `pip install waitress`
     # ? If you are willing to use waitress-serve command, please add `/home/sadm/.local/bin` to your ~/.bashrc
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=PORT)
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=PORT)
 
 
 
@@ -55,7 +55,7 @@ if __name__ == "__main__":
 # import json
 
 
-
+CORS(app)
 
 
 """
